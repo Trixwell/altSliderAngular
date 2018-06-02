4a850b0ab941ffc421a1be21ee385ef74a4a3639
Firebaba <ntbilieli@gmail.com>
2018-06-02 12:28:15 +0300

    Simple scrollbar for altSliderAngular.

--- try-it/default/altSlider.js
+++ try-it/default/altSlider.js
@@ -4,14 +4,21 @@ direct.directive('altSlider', function ($http) {
     return {
         templateUrl: 'slider.html',
         link: function (scope, element, attrs) {
-            scope.current_position = 1;
+            scope.current_position = 0;
             scope.display_elements_count = 3;
             scope.loadData = function () {
                 $http.get('test.json').then(function (response) {
                     scope.slides = response.data;
+                    scope.element_width = element[0].clientWidth;
+                    scope.new_position = scope.element_width / (scope.slides.length - scope.display_elements_count + 1);
+                    scope.myObj = {
+                        'width': scope.new_position + 'px'
+                    }
+
                     scope.updateScreen();
                 });
             }
+
             scope.loadData();
 
             scope.updateScreen = function () {
@@ -25,38 +32,33 @@ direct.directive('altSlider', function ($http) {
                 } else {
                     scope.moveRight();
                 }
-
                 scope.updateScreen();
                 scope.$apply();
-
 
             });
 
             scope.moveScroll = function () {
-                scope.left_side = document.querySelectorAll('.scroll_bar').clientWidth;
-                scope.left_size_bar = scope.current_position * scope.left_side;
-
-                scope.myObj = {
-                    'left': scope.left_size_bar
-                }
+                scope.left_size_bar = scope.current_position * scope.new_position;
+                scope.myObj.left = scope.left_size_bar + 'px';
             };
 
-            scope.moveRight = function () {
+            scope.moveRight = function (is_move) {
                 if (scope.current_position >= (scope.slides.length - scope.display_elements_count)) {
                     scope.current_position = scope.slides.length - scope.display_elements_count - 1;
                 }
 
                 scope.current_position++;
-                scope.moveScroll();
+                scope.moveScroll(is_move);
             };
 
-            scope.moveLeft = function () {
+            scope.moveLeft = function (is_move) {
                 if (scope.current_position <= 0) {
                     scope.current_position = 1;
                 }
 
+                scope.myObj.right = scope.left_size_bar + 'px';
                 scope.current_position--;
-                scope.moveScroll();
+                scope.moveScroll(is_move);
             };
         }
     }
